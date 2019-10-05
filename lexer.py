3401f0c0ce5848418ad0f0cfab082ce125fafc70
asynts <asynts@gmail.com>
2019-10-05 18:57:40 +0200

    Prototyping the parser.

--- lexer.py
+++ lexer.py
@@ -1,33 +1,46 @@
+import typing
+
+from dataclasses import dataclass
+
+@dataclass
+class Token:
+    value: typing.Any
+    offset: int
+
+
 class Lexer:
     def __init__(self, input_):
-        self._input = input_
+        self.input = input_
+        self._cursor = 0
 
     @property
     def has_more_input(self):
-        return len(self._input) > 0
+        return self._cursor < len(self.input)
 
     def peek(self):
-        if self.has_more_input:
-            return self._input[0]
-        raise IndexError
+        return self.input[self._cursor]
 
     def next(self):
-        if self.has_more_input:
-            self._input, value = self._input[1:], self._input[0]
-            return value
-        raise IndexError
+        value = self.peek()
+        self._cursor += 1
+
+        return value
 
     def next_integer(self):
+        token = Token(None, self._cursor)
+
         if self.has_more_input and self.peek() in '123456789':
             matched = self.next()
 
             while self.has_more_input and self.peek() in '0123456789':
                 matched += self.next()
 
-            return int(matched)
+            token.value = int(matched)
+            return token
         return None
 
     def next_special(self):
         if self.has_more_input and self.peek() in '+-*/()':
-            return self.next()
+            # Notice: Python evaluates function arguments from left to right.
+            return Token(offset=self._cursor, value=self.next())
         return None
--- main.py
+++ main.py
@@ -1,18 +1,68 @@
 #!/usr/bin/env python
 
+from dataclasses import dataclass
+
 from lexer import Lexer
 
 lx = Lexer('(20+1)*2')
 
+PRECEDENCE = {
+    '+': 1,
+    '-': 1,
+    '*': 2,
+    '/': 2,
+    '(': 0,
+}
+
+@dataclass
+class Expr:
+    pass
+
+@dataclass
+class ExprInteger(Expr):
+    value: int
+    offset: int
+
+@dataclass
+class ExprBinary(Expr):
+    lhs: Expr
+    rhs: Expr
+    op: str
+    offset: int
+
+operands = []
+operations = []
+
+def apply_():
+    rhs = operands.pop()
+    lhs = operands.pop()
+    op = operations.pop()
+
+    operands.append(ExprBinary(lhs, rhs, op.value, op.offset))
+
 while lx.has_more_input:
     node = lx.next_integer()
     if node is not None:
-        print(node)
+        operands.append(ExprInteger(node.value, node.offset))
         continue
 
     node = lx.next_special()
     if node is not None:
-        print(node)
+        if node.value == '(':
+            operations.append(node)
+        elif node.value == ')':
+            while len(operations) > 0 and operations[-1].value != '(':
+                apply_()
+            operations.pop()
+        else:
+            if len(operations) > 0 and PRECEDENCE[operations[-1].value] > PRECEDENCE[node.value]:
+                apply_()
+            operations.append(node)
         continue
 
     raise SyntaxError
+
+while len(operations) > 0:
+    apply_()
+
+print(operands)

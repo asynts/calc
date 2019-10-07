cb794fa52b052e0fe2bfe26f36c501eeabe4bd14
asynts <asynts@gmail.com>
2019-10-07 18:52:21 +0200

    Writing down the grammar.

--- lexer.py
+++ lexer.py
@@ -1,66 +1,33 @@
-p_identifier = p.regex('[_a-z0-9]+')
-p_function_arguments = p.maybe(p.chain(
-    p_expr, # ???
-    p.maybe(p.repeat(p.chain(
-        p.match(','),
-        p_expr
-    )))
-))
+"""
+Rules are matched from left to right.
+The first alternative takes precedence if there is a conflict.
+The star '*' operator is greedy.
 
-p_expr_integer = p.regex('[0-9]+')
-p_expr_variable = p.identifier()
-p_expr_invoke = p.chain(
-    p.identifier(),
-    p.match('('),
-    p.any(
-        p.chain(
-            p_function_arguments,
-            p.match(')')
-        ),
-        p.error('expected function arguments')
-    )
-)
-p_expr_prefix = p.chain(
-    p.any(
-        p.match('++'),
-        p.match('--'),
-        p.match('-')
-    ),
-    p.any(
-        p_expr,  # ???
-        p.error('expected expression')
-    )
-)
+<args> ::= <expr> (',' <expr>)* ;
 
-p_expr_simple = p.any(
-    p_expr_invoke,
-    p_expr_prefix,
+<expr> ::= <expr> INFIX <expr>
+        / PREFIX <expr>
+        / <expr> POSTFIX
+        /  '(' <expr> ')'
+        / INTEGER
+        / IDENTIFIER '(' <args>? ')'
+        / IDENTIFIER
+        ;
+"""
 
-    p_expr_integer,
-    p_expr_variable
-)
+"""
+This grammar accepts an input if and only if it would be accepted by the previous parser.
+The star '*' operator is greedy.
 
-p_expr_postfix = p.chain(
-    p_expr, # ???
-    p.any(
-        p.match('++'),
-        p.match('--')
-    )
-)
+<expr> ::= <term> INFIX <term> ;
 
-p_expr_infix = p.chain(
-    p_expr, # ???
-    p.any(
-        p.match('+'),
-        p.match('-'),
-        p.match('*'),
-        p.match('/'),
-    ),
-    p_expr # ???
-)
+<term> :: = PREFIX* <expr> POSTFIX* ;
 
-p_expr = p.any(
-    p_expr_postfix,
-    p_expr_infix,
-    p_expr_simple
-)
+<operand> ::= '(' <expr> ')'
+           / INTEGER
+           / IDENTIFIER '(' <args>? ')'
+           / IDENTIFIER
+           ;
+
+<args> ::= <expr> (',' <expr>)* ;
+"""

f3550f69d25e529b1689dc3694f2143ea9c6bba8
asynts <asynts@gmail.com>
2019-10-07 17:05:05 +0200

    Creating an outline for the lexer.
    There still is a huge open question: how to avoid the direct recursion
    from `p_expr`?

--- lexer.py
+++ lexer.py
@@ -1,28 +1,66 @@
-def unary_expr():
-    for op in ['-']:
-        if match(op):
-            emit(UNARY, op)
-            return expr()
-    return False
+p_identifier = p.regex('[_a-z0-9]+')
+p_function_arguments = p.maybe(p.chain(
+    p_expr, # ???
+    p.maybe(p.repeat(p.chain(
+        p.match(','),
+        p_expr
+    )))
+))
 
-def call_expr():
-    pass
+p_expr_integer = p.regex('[0-9]+')
+p_expr_variable = p.identifier()
+p_expr_invoke = p.chain(
+    p.identifier(),
+    p.match('('),
+    p.any(
+        p.chain(
+            p_function_arguments,
+            p.match(')')
+        ),
+        p.error('expected function arguments')
+    )
+)
+p_expr_prefix = p.chain(
+    p.any(
+        p.match('++'),
+        p.match('--'),
+        p.match('-')
+    ),
+    p.any(
+        p_expr,  # ???
+        p.error('expected expression')
+    )
+)
 
-def integer_expr():
-    pass
+p_expr_simple = p.any(
+    p_expr_invoke,
+    p_expr_prefix,
 
-def variable_expr():
-    pass
+    p_expr_integer,
+    p_expr_variable
+)
 
-def atomic_expr():
-    unary_expr() or call_expr() or integer_expr() or variable_expr()
+p_expr_postfix = p.chain(
+    p_expr, # ???
+    p.any(
+        p.match('++'),
+        p.match('--')
+    )
+)
 
-def infix_expr():
-    pass
+p_expr_infix = p.chain(
+    p_expr, # ???
+    p.any(
+        p.match('+'),
+        p.match('-'),
+        p.match('*'),
+        p.match('/'),
+    ),
+    p_expr # ???
+)
 
-def postfix_expr():
-    pass
-
-def expr():
-    pass
-
+p_expr = p.any(
+    p_expr_postfix,
+    p_expr_infix,
+    p_expr_simple
+)

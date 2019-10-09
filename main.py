8d8b16ca519815ea0a6cd14a1287118d19ae124c
asynts <asynts@gmail.com>
2019-10-09 15:25:57 +0200

    Moving the parser into it's own file.

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python
 
 import lang
+
+lexer = lang.lexer.Lexer('(20+1)*2')
+assert lexer._lex_expression()
+print(lexer._output)
+
+parser = lang.parser.Parser(lexer._output)
+print(parser.parse_expression())
